1f60a850c0d12180bf3275dc2604deb38a965411
Antonio Silva <antonio.silvajr23@gmail.com>
2019-10-25 15:00:15 -0300

    Add list of marker poops

--- app/src/main/java/com/discordtime/gpts/repository/PlaceRepository.kt
+++ app/src/main/java/com/discordtime/gpts/repository/PlaceRepository.kt
@@ -2,9 +2,12 @@ package com.discordtime.gpts.repository
 
 import androidx.lifecycle.LiveData
 import com.discordtime.gpts.listplaces.model.Place
+import com.discordtime.gpts.remote.FirestoreAPI
 import com.discordtime.gpts.remote.RemoteAPI
 
-class PlaceRepository(val remoteAPI: RemoteAPI<Place>): IPlaceRepository {
+class PlaceRepository: IPlaceRepository {
+
+    private val remoteAPI: RemoteAPI<Place> = FirestoreAPI()
 
     override fun getPlaces(): LiveData<List<Place>> {
         return remoteAPI.get()
