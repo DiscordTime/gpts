3887baa1bc9eb1596c95552fac37c52034c3840e
asilvaj <antonio.silvajr23@gmail.com>
2019-10-28 14:36:12 -0300

    Add markers on the map

--- app/src/main/java/com/discordtime/gpts/di/RemoteModule.kt
+++ app/src/main/java/com/discordtime/gpts/di/RemoteModule.kt
@@ -3,8 +3,12 @@ package com.discordtime.gpts.di
 import com.discordtime.gpts.listplaces.model.Place
 import com.discordtime.gpts.remote.FirestoreAPI
 import com.discordtime.gpts.remote.RemoteAPI
+import com.discordtime.gpts.tools.Constants.PLACES_COLLECTION
+import com.google.firebase.firestore.FirebaseFirestore
 import org.koin.dsl.module
 
 val remoteModules = module {
-    factory<RemoteAPI<Place>> { FirestoreAPI() }
+    single { FirebaseFirestore.getInstance() }
+    single {get<FirebaseFirestore>().collection(PLACES_COLLECTION) }
+    factory { FirestoreAPI(Place::class.java, get()) as RemoteAPI<Place> }
 }
--- app/src/main/java/com/discordtime/gpts/maps/viewmodel/MapViewModel.kt
+++ app/src/main/java/com/discordtime/gpts/maps/viewmodel/MapViewModel.kt
@@ -16,9 +16,8 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapViewModel: ViewModel() {
+class MapViewModel(val repository: IPlaceRepository): ViewModel() {
 
-    private val repository: IPlaceRepository = PlaceRepository()
     private lateinit var googleMap: GoogleMap
 
     private val placesData: LiveData<List<Place>> = repository.getPlaces()
